a449ec6cec6a1323c3fbec5731f0fe107132e612
Robin Saleh-Jan <robinsjdotcom@users.noreply.github.com>
2017-05-08 10:04:53 +0200

    Update rendering of content

--- apps/web/web/static/js/app.js
+++ apps/web/web/static/js/app.js
@@ -4,60 +4,60 @@ import Graph from "./graph";
 
 export var App = {
   run: function() {
-    let graph = new Graph();
+    let graph = new Graph(function(graph) {
+      new Cytoscape({
+        container: document.getElementById("cy"),
+        boxSelectionEnabled: false,
+        autounselectify: true,
 
-    var cytopscape = new Cytoscape({
-      container: document.getElementById("cy"),
-      boxSelectionEnabled: false,
-      autounselectify: true,
-
-      layout: {
-        name: 'cose',
-        ready: undefined,
-        stop: undefined,
-        animate: true,
-        animationThreshold: 100,
-        refresh: 20,
-        fit: true,
-        padding: 5,
-        boundingBox: undefined,
-        randomize: true,
-        componentSpacing: 100,
-        nodeOverlap: 0,
-        idealEdgeLength: function(edge) { return 400; },
-        edgeElasticity: function(edge) { return 500; },
-        nestingFactor: 0.25,
-        gravity: 10,
-        numIter: 50,
-        initialTemp: 10,
-        coolingFactor: 0.25,
-        minTemp: 1.0,
-        useMultitasking: true
-      },
-
-      style: [
-        {
-          selector: 'node',
-          style: {
-            'height': 70,
-            'width': 70,
-            'font-size': '40',
-            'color': 'white',
-            'background-color': 'white',
-            'label': 'data(id)'
-          }
+        layout: {
+          name: 'cose',
+          ready: undefined,
+          stop: undefined,
+          animate: true,
+          animationThreshold: 100,
+          refresh: 20,
+          fit: true,
+          padding: 5,
+          boundingBox: undefined,
+          randomize: true,
+          componentSpacing: 100,
+          nodeOverlap: 0,
+          idealEdgeLength: function(edge) { return 400; },
+          edgeElasticity: function(edge) { return 500; },
+          nestingFactor: 0.25,
+          gravity: 10,
+          numIter: 50,
+          initialTemp: 10,
+          coolingFactor: 0.25,
+          minTemp: 1.0,
+          useMultitasking: true
         },
 
-        {
-          selector: 'edge',
-          style: {
-            'width': 10,
-            'opacity': 1.0,
-            'line-color': '#40e0d0'
+        style: [
+          {
+            selector: 'node',
+            style: {
+              'height': 70,
+              'width': 70,
+              'font-size': '40',
+              'color': 'white',
+              'background-color': 'white',
+              'label': 'data(id)'
+            }
+          },
+
+          {
+            selector: 'edge',
+            style: {
+              'width': 10,
+              'opacity': 1.0,
+              'line-color': '#40e0d0'
+            }
           }
-        }
-      ],
-      elements: graph
-    })
+        ],
+        elements: graph
+      })
+    });
   }
 }
--- apps/web/web/static/js/graph.js
+++ apps/web/web/static/js/graph.js
@@ -1,54 +1,35 @@
 class Graph {
-  constructor() {
-    let request = this.send_request();
-    let response = request.response;
-    let obj = this.parse_request(response);
-    let representation = this.construct_graph(obj);
-    return representation;
-  }
-
-  send_request() {
+  constructor(callback) {
     let request = new XMLHttpRequest();
-    request.open("GET", "https://trendinghashtaggraph.herokuapp.com/api/graph", false);
-    request.send();
-    return request;
-  }
+    request.open("GET", "https://trendinghashtaggraph.herokuapp.com/api/graph", true);
 
-  parse_request(response) {
-    if (typeof response !== 'string') {
-      let msg = `Expected: string\nGot: ${typeof(data)}`
-      throw new TypeError(msg);
-    }
-    let obj = JSON.parse(response);
-    return obj;
-  }
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        let response = request.response;
+        let json = JSON.parse(response);
 
-  construct_graph(data) {
-    if (typeof data !== 'object') {
-      let msg = `Expected: object\nGot: ${typeof(data)}`
-      throw new TypeError(msg);
-    }
+        let graph = [];
+        for (let v of json) {
+          var hashtag = {data: {id: v.hashtag}};
+          graph.push(hashtag);
 
-    let elements = [];
+          for (let e of v.edges) {
+            let edge = {data: {id: e}};
+            graph.push(edge);
 
-    for (let v of data) {
-      var hashtag = {data: {id: v.hashtag }};
-      elements.push(hashtag);
+            let id_hashtag = hashtag.data.id.charAt(3);
+            let id_edge = edge.data.id.charAt(6);
+            let id_str = `${id_hashtag}->${id_edge}`;
 
-      for (let e of v.edges) {
-        let edge = {data: { id: e }};
-        elements.push(edge);
-
-        let id_hashtag = hashtag.data.id.charAt(3);
-        let id_edge = edge.data.id.charAt(6);
-        let id_str = `${id_hashtag}->${id_edge}`;
-
-        let connection = {data: { id: id_str, source: hashtag.data.id, target: edge.data.id}};
-        elements.push(connection);
+            let connection = {data: { id: id_str, source: hashtag.data.id, target: edge.data.id}};
+            graph.push(connection);
+          }
+        }
+        callback(graph);
       }
     }
-    return elements;
+
+    request.send();
   }
 }
-
 export {Graph as default};
