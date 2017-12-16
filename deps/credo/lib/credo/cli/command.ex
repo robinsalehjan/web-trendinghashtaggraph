8b5e2e88a3ebca86c890876325a0be46fb5ba67d
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2017-12-16 13:14:12 +0100

    Remove build artifacts

--- deps/credo/lib/credo/cli/command.ex
+++
@@ -1,8 +0,0 @@
-defmodule Credo.CLI.Command do
-  @doc false
-  defmacro __using__(_opts) do
-    quote do
-      Module.register_attribute(__MODULE__, :shortdoc, persist: true)
-    end
-  end
-end
