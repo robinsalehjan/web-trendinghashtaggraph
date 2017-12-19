77a2b3b7b9615a67b37cb63cc7b1ecf2a980ecc0
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2017-12-19 20:42:52 +0100

    Update API client endpoint

--- apps/web/web/static/js/graph.js
+++ apps/web/web/static/js/graph.js
@@ -1,7 +1,7 @@
 class Graph {
   constructor(callback) {
     let request = new XMLHttpRequest();
-    request.open("GET", "http://35.189.118.122/api/graph", true);
+    request.open("GET", "http://35.197.213.243/api/graph", true);
 
     request.onreadystatechange = function() {
       if (request.readyState == 4 && request.status == 200) {
